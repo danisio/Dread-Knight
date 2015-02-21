20f4dacabad1a1b7f5da1cc533a4f175d5a0c1c1
danisio <danisio83@gmail.com>
2015-02-21 23:57:10 +0200

    added SinglePlayer shooting and scoring

--- Dread Knight/SinglePlayer.cs
+++ Dread Knight/SinglePlayer.cs
@@ -7,26 +7,29 @@ using System.Threading.Tasks;
 
 namespace Dread_Knight
 {
-  
     class SinglePlayer
-    {  struct Object
     {
-        public int x;
-        public int y;
-        public string str;
-        public ConsoleColor color;
-    }
+        struct Object
+        {
+            public int x;
+            public int y;
+            public string str;
+            public ConsoleColor color;
+        }
 
         static Object ourPlayer = new Object();
         static Random randomGenerator = new Random();
         static List<Object> enemies = new List<Object>();
+        static List<Object> shots = new List<Object>();
+
+        static int score = 0;
         //static int livesCount = 5;
 
         internal static void SinglePlay()
         {
-            ourPlayer.x = 1;
+            Console.OutputEncoding = Encoding.UTF8;
 
-            //make our playern
+            //make our player
             ourPlayer.x = 0;
             ourPlayer.y = Console.WindowHeight / 2;
             ourPlayer.str = " ('0.0)-=╦╤── ";
@@ -34,8 +37,12 @@ namespace Dread_Knight
 
             int step = 0;
             int enemiesPause = 5;
+
             while (true)
             {
+                //check if an enemy is hitted by shot
+                CollisionShotAndEnemy();
+
                 //add new enemy every 3 steps
                 if (step % enemiesPause == 0)
                 {
@@ -44,18 +51,24 @@ namespace Dread_Knight
                 }
                 step++;
 
-                //move our player(key pressed)
+                //move our player and shoot(key pressed)
                 while (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo pressedKey = Console.ReadKey(true);
                     MoveOurPlayer(pressedKey);
                 }
 
+                MoveShots();
+
                 MoveEnemies();
 
-                //clear the console
+                //check if an enemy is hitted by shot
+                CollisionShotAndEnemy();
+
+                //clear the console - old positions
                 Console.Clear();
 
+                //draw new positions
                 RedrawPlayfield();
 
                 //draw info
@@ -63,9 +76,51 @@ namespace Dread_Knight
 
                 //slow down program
                 Thread.Sleep(100);
-
             }
         }
+
+        static void CollisionShotAndEnemy()
+        {
+            List<object> enemiesToRemove = new List<object>();
+            List<object> shotsToRemove = new List<object>();
+
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                for (int j = 0; j < shots.Count; j++)
+                {
+                    if (enemies[i].x == shots[j].x && enemies[i].y == shots[j].y)
+                    {
+                        enemiesToRemove.Add(enemies[i]);
+                        shotsToRemove.Add(shots[j]);
+                        score += 10;
+                    }
+                }
+            }
+
+            List<Object> newListOfEnemies = new List<Object>();
+            List<Object> newListOfShots = new List<Object>();
+
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                if (!enemiesToRemove.Contains(enemies[i]))
+                {
+                    newListOfEnemies.Add(enemies[i]);
+                }
+            }
+
+            enemies = newListOfEnemies;
+
+            for (int i = 0; i < shots.Count; i++)
+            {
+                if (!shotsToRemove.Contains(shots[i]))
+                {
+                    newListOfShots.Add(shots[i]);
+                }
+            }
+
+            shots = newListOfShots;
+        }
+
         static void AddNewEnemy()
         {
             Object newEnemy = new Object();
@@ -75,6 +130,16 @@ namespace Dread_Knight
             newEnemy.color = ConsoleColor.Yellow;
             enemies.Add(newEnemy);
 
+        }
+
+        static void Shoot()
+        {
+            Object newShot = new Object();
+            newShot.x = ourPlayer.x + 1 + 14;   //14 = size of our player
+            newShot.y = ourPlayer.y;
+            newShot.str = "Ѿ";
+            newShot.color = ConsoleColor.Black;
+            shots.Add(newShot);
         }
 
         static void MoveOurPlayer(ConsoleKeyInfo pressedKey)
@@ -94,6 +159,32 @@ namespace Dread_Knight
 
                 }
             }
+            else if (pressedKey.Key == ConsoleKey.Spacebar)
+            {
+                Shoot();
+            }
+        }
+
+        static void MoveShots()
+        {
+            List<Object> newListOfShots = new List<Object>();
+            for (int i = 0; i < shots.Count; i++)
+            {
+                Object oldShot = shots[i];
+                Object newShot = new Object();
+                newShot.x = oldShot.x + 1;
+                newShot.y = oldShot.y;
+                newShot.str = oldShot.str;
+                newShot.color = oldShot.color;
+
+                if (newShot.x < Console.WindowWidth)
+                {
+                    newListOfShots.Add(newShot);
+                }
+            }
+
+
+            shots = newListOfShots;
         }
 
         static void MoveEnemies()
@@ -112,6 +203,8 @@ namespace Dread_Knight
                     //livesCount--;
                     Console.Beep(1000, 50);
                     enemies.Clear();
+                    shots.Clear();
+
                     // console.writeline environment.exit(0)
                 }
 
@@ -127,11 +220,15 @@ namespace Dread_Knight
         static void RedrawPlayfield()
         {
             PrintOnPosition(ourPlayer.x, ourPlayer.y, ourPlayer.str, ourPlayer.color);
+
             foreach (Object enemy in enemies)
             {
                 PrintOnPosition(enemy.x, enemy.y, enemy.str, enemy.color);
             }
-
+            foreach (Object shot in shots)
+            {
+                PrintOnPosition(shot.x, shot.y, shot.str, shot.color);
+            }
         }
 
         static void PrintOnPosition(int x, int y, string str, ConsoleColor color = ConsoleColor.Gray)
@@ -141,5 +238,4 @@ namespace Dread_Knight
             Console.Write(str);
         }
     }
-
 }
