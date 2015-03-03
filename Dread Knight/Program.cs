5a1792e9e73ff5f789167d2ba7dc7318d04b3e17
YordanGergov <yordan.gergov91@gmail.com>
2015-03-03 23:34:34 +0200

    music

--- Dread Knight/Program.cs
+++ Dread Knight/Program.cs
@@ -8,7 +8,7 @@ namespace Dread_Knight
     {
         static void Main()
         {
-            SoundPlayer sound = new SoundPlayer("../../Music/intro.wav");
+            SoundPlayer sound = new SoundPlayer("../../intro.wav");
             sound.PlayLooping();
             Console.BufferHeight = Console.WindowHeight = 35;
             Console.BufferWidth = Console.WindowWidth = 125;
